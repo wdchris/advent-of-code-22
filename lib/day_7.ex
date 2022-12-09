0b6b593216999e5d81a80930635fbb3d96f52d6a
Chris Harding <chris.harding@cazoo.co.uk>
2022-12-09 14:06:20 +0000

    day seven, part one inital test

--- lib/day_7.ex
+++ lib/day_7.ex
@@ -1,5 +1,42 @@
 defmodule Day7 do
+  defmodule Directory do
+    defstruct [
+      :name,
+      :path
+    ]
+  end
+
+  defmodule File do
+    defstruct [
+      :name,
+      :path,
+      :parent,
+      size: 0
+    ]
+  end
+
   @input_file "resources/day_7_input.dat"
 
+  def part_one(input \\ File.read!(@input_file)) do
 
+  end
+
+  def create_folder_map([], folder_map, _), do: folder_map
+  def create_folder_map([head | tail], folder_map, current_dir) do
+    cond do
+      head == "$ cd /" -> create_folder_map(tail, folder_map, "/")
+      head == "$ ls" -> create_folder_map(tail, folder_map, current_dir)
+      String.starts_with?(head, "dir") ->
+        %{"name" => name } = Regex.named_captures(~r/dir (?<name>\w+)/, head)
+        updated_map =
+          Map.put(
+            folder_map,
+            current_dir <> name,
+            %Directory{
+              name: name,
+              path: current_dir
+          })
+        create_folder_map(tail, updated_map, current_dir)
+    end
+  end
 end
--- test/day_7_test.exs
+++ test/day_7_test.exs
@@ -1,3 +1,95 @@
 defmodule DaySevenTest do
   use ExUnit.Case
+
+  describe "part_one" do
+    test "sums up dirs <= 100000 correctly" do
+      input =
+        "$ cd /
+        $ ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        $ cd a
+        $ ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        $ cd e
+        $ ls
+        584 i
+        $ cd ..
+        $ cd ..
+        $ cd d
+        $ ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k"
+
+        assert Day7.part_one(input) == 95437
+    end
+  end
+
+  describe "create_folder_map" do
+    test "adds a directory" do
+      input = [
+        "$ cd /",
+        "$ ls",
+        "dir a"
+      ]
+
+      input_map = %{
+        "/" => %Day7.Directory{
+          name: "/",
+          path: "",
+        }
+      }
+
+      result = %{
+        "/" => %Day7.Directory{
+          name: "/",
+          path: "",
+        },
+        "/a" => %Day7.Directory{
+          name: "a",
+          path: "/",
+        }
+      }
+
+      assert Day7.create_folder_map(input, input_map, "") == result
+    end
+
+    # test "adds nested directories" do
+    #   input = [
+    #     "$ cd /",
+    #     "$ ls",
+    #     "dir a",
+    #     "$ cd a",
+    #     "$ ls",
+    #     "dir e"
+    #   ]
+
+    #   input_map = %{
+    #     "/" => %Day7.Directory{
+    #       name: "/",
+    #       path: "",
+    #     }
+    #   }
+
+    #   result = %{
+    #     "/" => %Day7.Directory{
+    #       name: "/",
+    #       path: "",
+    #     },
+    #     "/a" => %Day7.Directory{
+    #       name: "a",
+    #       path: "/",
+    #     }
+    #   }
+
+    #   assert Day7.create_folder_map(input, input_map, "") == result
+    # end
+  end
 end
